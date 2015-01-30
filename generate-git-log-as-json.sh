17b419d3c55195c691fe90666fb261b9fdfafb4a
Roberto <robertooliveiraestudo@gmail.com>
2015-01-30 01:08:12 -0200

    Atualizando o gitignore e atualizando o arquivo que gera o JSON do git log.

---
+++ generate-git-log-as-json.sh
@@ -0,0 +1,1 @@
+git log --pretty=format:'{%n "commit": "%H",%n "author": "%an <%ae>",%n "date": "%ad",%n "message": "%f"%n},' $@ | perl -pe 'BEGIN{print "["}; END{print "]\n"}' | perl -pe 's/},]/}]/'
